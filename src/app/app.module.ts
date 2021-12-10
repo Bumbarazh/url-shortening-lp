b17f133a3ae3521734d7e99cf0e80fe150d686f7
bumbarazh <kos.shynkaruk@gmail.com>
2021-12-10 23:56:39 +0200

    added top and login menu components, styled them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { TopMenuComponent } from './components/top-menu/top-menu.component';
+import { LoginMenuComponent } from './components/login-menu/login-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    TopMenuComponent,
+    LoginMenuComponent
   ],
   imports: [
     BrowserModule
